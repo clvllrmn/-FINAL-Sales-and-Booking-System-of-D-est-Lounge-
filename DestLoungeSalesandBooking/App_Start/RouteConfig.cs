e25f579cbd4dae1d27cb6271430fe14a96b7d899
heatherryannabon <heather.ryann.abon@gmail.om>
2026-04-25 17:02:04 +0800

    route fix

--- DestLoungeSalesandBooking/App_Start/RouteConfig.cs
+++ DestLoungeSalesandBooking/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DestLoungeSalesandBooking
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Main", action = "AdminNailDesk", id = UrlParameter.Optional }
+                defaults: new { controller = "Main", action = "Homepage", id = UrlParameter.Optional }
             );
         }
     }
